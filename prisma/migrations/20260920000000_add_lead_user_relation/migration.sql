bad040fb592e352bfdc3c87daabbde5141caf08d
Ayhsa Urmi <ayshaismail021@gmail.com>
2026-09-25 18:59:54 +0600

    fix: align prisma migration history

--- prisma/migrations/20260920000000_add_lead_user_relation/migration.sql
+++ prisma/migrations/20260920000000_add_lead_user_relation/migration.sql
@@ -5,8 +5,4 @@ ALTER TABLE "Lead" ADD COLUMN "userId" TEXT;
 CREATE INDEX "Lead_userId_idx" ON "Lead"("userId");
 
 -- AddForeignKey
-ALTER TABLE "Lead"
-ADD CONSTRAINT "Lead_userId_fkey"
-FOREIGN KEY ("userId") REFERENCES "User"("id")
-ON DELETE SET NULL
-ON UPDATE CASCADE;
+ALTER TABLE "Lead" ADD CONSTRAINT "Lead_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
